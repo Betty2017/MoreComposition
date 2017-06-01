3db3246452a6d33139adfa9e3a7c675db7409a92
Betty Negatu <Betty Negatu>
2017-05-31 22:39:56 -0400

    First Commit

---
+++ src/Car.java
@@ -0,0 +1,12 @@
+
+public class Car {
+
+public String accelerate()
+	{
+	return"The car is accelerating";
+	}
+public String setSpeed(){
+	return"The Speed is 90 mph";	
+	
+}
+}
---
+++ src/Porsche.java
@@ -0,0 +1,20 @@
+
+public class Porsche extends Car {
+	public String start(){
+	 return"The red Porsche is starting";
+ }
+	
+	@Override
+	public String accelerate(){
+	return "The red Porsche is accelerating"; 
+}@Override
+	public String setSpeed(){
+		return "The red Porsche is going 50 mph"; 
+	}
+	public String stop(){
+		return "The red Porsche is stopped"; 
+	}
+	
+	
+ 	
+}
